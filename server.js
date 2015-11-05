11bb0e8fc0325f0905815b7a3a4f656dd72a907a
Petermuturi <pmuturi65@gmail.com>
2015-11-05 19:21:52 +0300

    changes

--- server.js
+++ server.js
@@ -4,39 +4,14 @@ var express = require ('express');
 
 app.use(express.static(__dirname + "/public"));
 	
-// app.get('/products', function(req, res){
-//    console.log("I recieved");
-   
-//    hair = {
-// 		image : "image1",
-// 		description : "description1",
-// 		price : "cost1", 
-// 		tax : "tax1"
-// 	};
-// 	body = {
-// 		image : "image2",
-// 		description : "description2",
-// 		price : "cost2", 
-// 		tax : "tax2"
-// 	};
-// 	face = {
-// 		image : "image3",
-// 		description : "description3",
-// 		price : "cost3", 
-// 		tax : "tax3"
-// 	};
-
-// 	var products = [hair, body, face];
-// 	res.json(products);
-// });
 
 var server = http.createServer(function(req, res){
 	res.writeHead(200, {'Content-Type': 'text/html'});
 	res.end(app);
 });
 
-var port = Number(process.env.PORT || 8081);
+var port = Number(process.env.PORT || 8000);
 
-server.listen(8000);
+server.listen(8011);
  app.listen(port);
  console.log("Server by Peter");
